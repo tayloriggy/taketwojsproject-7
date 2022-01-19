ddba3867fa0f738e4e68820e7ac8e30d9e68940c
Iggy36 <42382841+tayloriggy@users.noreply.github.com>
2022-01-18 21:45:25 -0600

    Created index.html, style.css, and script.js files

---
+++ script.js
@@ -0,0 +1,11 @@
+/*
+ou should have a form where a user is able to input the cost of the meal.
+-The form should also let the user choose the percentage tip they want to give.
+-When they submit the form, show them the total with tip added.
+-Display the total tip amount as well so they know how much they're tipping.
+
+
+
+
+
+*/
